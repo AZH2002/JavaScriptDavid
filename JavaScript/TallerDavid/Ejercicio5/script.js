6cf78ef8ad393a7f8fcd44c8182d5e41d83cf1c2
AZH2002 <73726592+AZH2002@users.noreply.github.com>
2021-12-13 16:47:50 -0500

    Delete JavaScript directory

--- JavaScript/TallerDavid/Ejercicio5/script.js
+++
@@ -1,3 +0,0 @@
-var Operacion=32**6;
-
-console.log("El resultado de la Operacion es: ",Operacion);
--- JavaScript/js/script.js
+++
@@ -1,5 +0,0 @@
-
-var base = prompt("Ingrese el valor de la base");
-var altura = prompt("Ingrese el valor de la altura");
-var area = (base * altura)/2;
-document.writeln('El área del triángulo es: '+ area);
